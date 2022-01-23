7ec3516c43a81d94b1ae1c57d997da79ae418322
ishaansarna <ishaansarna23@gmail.com>
2022-01-23 15:29:34 +0530

    Initial Commit

---
+++ White House Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  White House Petitions
+//
+//  Created by Ishaan Sarna on 23/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
